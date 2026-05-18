e47c9971e884a3b46b4ac8a675eaa619e52f07e5
theAyMaN-ctrl <ayushtripathy2004@gmail.com>
2026-05-18 12:42:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,3 @@ VanillaTilt.init(document.querySelector(".container"), {
   glare: true,
   "max-glare": 0.2
 });
-
-// Built this calculator following this guide: https://www.youtube.com/watch?v=NhcZh8Bwr30&t=5s
